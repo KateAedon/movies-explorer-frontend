5839b0e90dc395481db445e1024cb0a3f7183aaa
Ekaterina Aedonitskaia <kate.aedon@gmail.com>
2021-06-08 22:26:14 +0200

    create file sctructure

---
+++ README.old.md
@@ -0,0 +1,4 @@
+# movies-explorer-frontend
+Final study project at YandexPraktikum
+https://kateaedon.movie.nomoredomains.icu
+IP 130.193.37.101
---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Main from '../Main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Movie explorer</h1>
+      <Main></Main>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
